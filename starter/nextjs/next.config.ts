2ec52625f22d48ceb46bfbaf7a7e3bc87c294af9
xehrad <colondashd@gmail.com>
2025-12-30 03:02:05 +0330

    [fix] update nextjs folde

--- starter/nextjs/next.config.ts
+++ starter/nextjs/next.config.ts
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+
   output: "standalone",
   reactStrictMode: true,
   typescript: {
